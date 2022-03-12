62c6f61aaa995ce4d89ccd9f0245925ec48fd675
Ritesh-Aggarwal <riteshaggarwal2001@gmail.com>
2022-03-12 16:46:08 +0530

    Update settings.py

--- arike/settings.py
+++ arike/settings.py
@@ -35,7 +35,7 @@ DATABASES = {
 }
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1",]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1","arike-app.herokuapp.com"]
 
 
 # Application definition
